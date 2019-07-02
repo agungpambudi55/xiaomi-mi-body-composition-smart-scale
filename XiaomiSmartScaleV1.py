763d309499aa69b8a7ce629c631ee722c13a879e
agungpambudi55 <agung.pambudi5595@gmail.com>
2019-07-02 14:20:52 +0700

    update

---
+++ XiaomiSmartScaleV1.py
@@ -0,0 +1,51 @@
+'''
+Created on Jul 2019
+'''
+
+import pygatt
+from XiaomiSmartScaleBodyMetrics import bodyMetrics
+
+import logging
+logging.basicConfig()
+logging.getLogger('pygatt').setLevel(logging.DEBUG)
+
+def handleData(handle, value):
+    if len(value) == 13:
+        emptyLoad = value[1] & (1<<7)
+        isStabilized = value[1] & (1<<5)
+        hasImpedance = value[1] & (1<<1)
+        impedance = ((value[10] & 0xFF) << 8) | (value[9] & 0xFF)
+        weight = (((value[12] & 0xFF) << 8) | (value[11] & 0xFF)) / 200.0
+
+        if emptyLoad:
+            print('Empty load')
+        else:
+            print('Weight {} kg'.format(weight))    
+
+try:
+    adapter = pygatt.GATTToolBackend(hci_device='hci0')
+    adapter.start()
+
+    for discover in adapter.scan(run_as_root=True, timeout=5):
+        if discover['name'] == 'MIBCS':
+            try:
+                print('Device found, try to connect with device')
+                device = adapter.connect(discover['address'], address_type=pygatt.BLEAddressType.random)
+                print('Connected with device')
+                                
+                while True:
+                    device.subscribe('00002a9c-0000-1000-8000-00805f9b34fb', callback=handleData)
+
+            except KeyboardInterrupt:
+                print('Terminate')
+            except:
+                print('Failed to connect with device')
+            finally:
+                device.disconnect()
+                
+except KeyboardInterrupt:
+    print('Terminate')
+except:
+    print('Something went wrong with adapter')
+finally:
+    adapter.stop()
